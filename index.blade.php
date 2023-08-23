0d36568234145d001630c324c1865dd2e7182da6
huyentrang04012003 <139440439+huyentrang04012003@users.noreply.github.com>
2023-08-22 21:54:39 -0700

    Add files via upload

--- index.blade.php
+++ index.blade.php
@@ -2,8 +2,8 @@
  
 @section('body')
     <div class="d-flex align-items-center justify-content-between">
-        <h1 class="mb-0">List Ingredients</h1>
-        <a href="{{ route('imports.create') }}" class="btn btn-primary">Add Ingredient</a>
+        <h1 class="mb-0">Manage Cod product</h1>
+        <a href="{{ route('management.create') }}" class="btn btn-primary">Add Reamin</a>
     </div>
     <hr />
     @if(Session::has('success'))
@@ -17,21 +17,23 @@
                 <th>No</th>
                 <th>Ingredient</th>
                 <th>Quantitive</th>
+                <th>Remain</th>
                 <th>Action</th>
             </tr>
         </thead>
         <tbody>
-            @if($imports->count() > 0)
-                @foreach($imports as $rs)
+            @if($management->count() > 0)
+                @foreach($management as $rs)
                     <tr>
                         <td class="align-middle">{{ $loop->iteration }}</td>
                         
                         <td class="align-middle">{{ $rs->ingredient }}</td>
                         <td class="align-middle">{{ $rs->quantity_import }}</td>
+                        <td class="align-middle">{{ $rs->Remain }}</td>
                         <td class="align-middle">
                             <div class="btn-group" role="group" aria-label="Basic example">
-                                <a href="{{ route('imports.show', $rs->id) }}" type="button" class="btn btn-secondary">Detail</a>
-                                <a href="{{ route('imports.edit', $rs->id)}}" type="button" class="btn btn-warning">Edit</a>
+                               
+                                
                                 <form action="{{ route('imports.destroy', $rs->id) }}" method="POST" type="button" class="btn btn-danger p-0" onsubmit="return confirm('Delete?')">
                                     @csrf
                                     @method('DELETE')
@@ -43,7 +45,7 @@
                 @endforeach
             @else
                 <tr>
-                    <td class="text-center" colspan="5">Ingredient not found</td>
+                    <td class="text-center" colspan="5">Nothing remain</td>
                 </tr>
             @endif
         </tbody>
